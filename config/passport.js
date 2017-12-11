263c6f9266d01c1fbc785570336c8823f0669861
ARJUN MAINI <you@example.com>
2017-12-11 14:40:38 +0530

    First commit

--- config/passport.js
+++ config/passport.js
@@ -12,16 +12,16 @@ module.exports = function(passport){
             callbackURL:'/auth/google/callback',
             proxy: true
         }, function (accessToken, refreshToken, profile, done){
-            // console.log(accessToken);
-            // console.log(profile);
+            console.log(accessToken);
+            console.log(profile);
 
             const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
 
             const newUser = {
                 googleID: profile.id,
-                firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                email: profile.emails[0].value,
+                firstName: profile._json.name.givenName,
+                lastName: profile._json.name.familyName,
+                email: profile._json.emails[0].value,
                 image: image
             }
 
